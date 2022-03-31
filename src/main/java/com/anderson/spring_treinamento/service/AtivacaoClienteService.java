1323f195e756b23090311157c0c906abc8699fff
asc <silvaandersonpi@gmail.com>
2022-03-30 22:48:58 -0300

    dependência opcional

--- src/main/java/com/anderson/spring_treinamento/service/AtivacaoClienteService.java
+++ src/main/java/com/anderson/spring_treinamento/service/AtivacaoClienteService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AtivacaoClienteService {
-    @Autowired
+    @Autowired(required = false)
     private Notificador notificador;
 
     public void ativar(Cliente cliente){
