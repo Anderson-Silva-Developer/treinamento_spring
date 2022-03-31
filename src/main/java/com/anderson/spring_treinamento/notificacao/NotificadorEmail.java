80b100cb682d63251ed7289bbf8e200745ffa2b8
asc <silvaandersonpi@gmail.com>
2022-03-30 22:44:07 -0300

    conhecendo os pontos de injeção e anotação @Autowired

--- src/main/java/com/anderson/spring_treinamento/notificacao/NotificadorEmail.java
+++ src/main/java/com/anderson/spring_treinamento/notificacao/NotificadorEmail.java
@@ -2,26 +2,14 @@ package com.anderson.spring_treinamento.notificacao;
 
 
 import com.anderson.spring_treinamento.entities.Cliente;
+import org.springframework.stereotype.Component;
 
 
+@Component
 public class NotificadorEmail implements Notificador{
-    private boolean caixaaAlta;
-    private  String hostServidorSmtp;
-
-    public NotificadorEmail(String hostServidorSmtp) {
-        this.hostServidorSmtp = hostServidorSmtp;
-
-    }
-
-    public void setCaixaaAlta(boolean caixaaAlta) {
-        this.caixaaAlta = caixaaAlta;
-    }
 
     @Override
     public void notificar(Cliente cliente, String mensagem){
-        if(this.caixaaAlta){
-         mensagem = mensagem.toUpperCase();
-        }
-        System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n",cliente.getTelefone(),cliente.getEmail(),this.hostServidorSmtp,mensagem);
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",cliente.getTelefone(),cliente.getEmail(),mensagem);
     }
 }
--- src/main/java/com/anderson/spring_treinamento/service/AtivacaoClienteService.java
+++ src/main/java/com/anderson/spring_treinamento/service/AtivacaoClienteService.java
@@ -3,17 +3,14 @@ package com.anderson.spring_treinamento.service;
 
 import com.anderson.spring_treinamento.entities.Cliente;
 import com.anderson.spring_treinamento.notificacao.Notificador;
-import com.anderson.spring_treinamento.notificacao.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
+@Component
 public class AtivacaoClienteService {
-
+    @Autowired
     private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-    }
 
     public void ativar(Cliente cliente){
         cliente.Ativar();
