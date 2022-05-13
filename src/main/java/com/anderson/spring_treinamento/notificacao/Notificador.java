b74e67e04c1e54a9cd1a217158bf19985a03c74b
asc <silvaandersonpi@gmail.com>
2022-05-13 11:46:30 -0300

    aula JPA

--- src/main/java/com/anderson/spring_treinamento/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.anderson.spring_treinamento.notificacao;
-
-import com.anderson.spring_treinamento.entities.Cliente;
-
-public interface Notificador {
-    void notificar(Cliente cliente,String mensagem);
-}
